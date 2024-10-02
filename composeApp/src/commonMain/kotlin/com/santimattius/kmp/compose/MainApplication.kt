1721e61ea98dd34fa8c5af058d6d5b6da100272e
Santiago Mattiauda <santimattius@gmail.com>
2024-10-02 20:49:50 -0300

    [fix] fix koin

--- composeApp/src/commonMain/kotlin/com/santimattius/kmp/compose/MainApplication.kt
+++ composeApp/src/commonMain/kotlin/com/santimattius/kmp/compose/MainApplication.kt
@@ -4,15 +4,10 @@ import androidx.compose.runtime.Composable
 import com.santimattius.kmp.compose.di.applicationModules
 import com.santimattius.kmp.compose.navigation.Navigation
 import org.koin.compose.KoinApplication
-import org.koin.dsl.koinApplication
-
-fun koinConfiguration() = koinApplication {
-    modules(applicationModules())
-}
 
 @Composable
 fun MainApplication() {
-    KoinApplication(::koinConfiguration) {
+    KoinApplication(application = { modules(applicationModules()) }) {
         Navigation()
     }
 }
